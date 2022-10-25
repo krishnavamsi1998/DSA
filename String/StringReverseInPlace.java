409ecd90f15b0a94d82222ee2a939a745ffa2f06
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-10-25 10:11:15 +0530

    Create StringReverseInPlace.java

---
+++ String/StringReverseInPlace.java
@@ -0,0 +1,18 @@
+//O(n),O(1)
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int n=s.length;
+        int i=0;
+        int j=n-1;
+        
+        while(i<=j)
+        {
+            char t=s[i];
+            s[i++]=s[j];
+            s[j--]=t;
+
+        }        
+        
+    }
+}
