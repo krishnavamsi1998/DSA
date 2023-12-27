4869a37439d7a4e92913c228c3b115933d01f574
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2023-12-27 12:12:16 +0530

    Update NoOf1BitsLC191.java

--- Bit Manipulation/NoOf1BitsLC191.java
+++ Bit Manipulation/NoOf1BitsLC191.java
@@ -1,1 +1,19 @@
+//O(1),O(1)
+//neetcode
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
 
+        int count=0;
+
+        while(n!=0)
+        {
+            n=n & (n-1);
+            count++;
+          
+        }
+
+        return count;
+        
+    }
+}
