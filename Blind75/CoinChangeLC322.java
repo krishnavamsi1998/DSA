96a38ba755d28c23a50786c1c83f51c5e329bc24
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2023-12-31 23:14:15 +0530

    Create CoinChangeLC322.java

---
+++ Blind75/CoinChangeLC322.java
@@ -0,0 +1,26 @@
+//O(amount*no of coins), O(amount)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        int[] dp=new int[amount +1 ];
+
+        // fill with amount +1
+        for(int i=0;i<amount+1;i++)        
+            dp[i]=amount+1; 
+
+        dp[0]=0;         
+        
+        for(int i=1;i<amount+1;i++)
+        {
+            for(int j=0;j<coins.length;j++)
+            {
+                if(i-coins[j]>=0)
+                    dp[i]=Math.min(dp[i], 1+ dp[i-coins[j]]);
+            }
+        }
+
+        if(dp[amount]==amount+1)
+            return -1;
+        return dp[amount];
+    }
+}
