4dd04d70e0e42d7037c8630e8ed26b8ae78d05a9
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2024-12-17 19:14:46 +0530

    Update RemoveElementLC27.java

--- LC150/Two Pointer/RemoveElementLC27.java
+++ LC150/Two Pointer/RemoveElementLC27.java
@@ -1,1 +1,19 @@
+//O(n),O(1)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // k : pos to swap
+        int k = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            // found a non val ele replace in k else ignore
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+
+        }
+        // after inserting all k is the no of ele that are non val
+        return k;
+
+    }
+}
