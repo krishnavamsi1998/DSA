7ed438080c85ce5b511e7e098948cd5bb26570d9
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2024-12-19 16:50:18 +0530

    Create JumpGame2LC45.java

---
+++ LC150/Greedy/JumpGame2LC45.java
@@ -0,0 +1,26 @@
+//O(n),O(1)
+//BFS
+//neetcode
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int l=0;
+        int r=0;
+        int res=0;
+        while(r<n-1)
+        {
+            int farthest=0;
+            for(int i=l;i<r+1;i++)
+            {
+                farthest=Integer.max(farthest,i+nums[i]);
+
+            }
+            l=r+1;
+            r=farthest;
+            res=res+1;
+
+        }
+        return res;
+
+    }
+}
